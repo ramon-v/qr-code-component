6e5b5884231d8915190435e345cb82ad19088566
Ramon Vasconcelos <ramonv.oliveira@gmail.com>
2023-02-24 21:20:45 -0300

    passing props to component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,11 @@ import QRCode from './components/QRCode'
 function App() {
   return (
     <div className="w-full h-full flex justify-center items-center bg-light-gray">
-      <QRCode />
+      <QRCode
+        title="Improve your front-end skills by building projects"
+        description="Scan the QR code to visit Frontend Mentor and take your coding skills to the next level"
+        imageUrl="./images/image-qr-code.png"
+      />
     </div>
   )
 }
--- src/components/QRCode.tsx
+++ src/components/QRCode.tsx
@@ -1,11 +1,21 @@
-function QRCode() {
+import { QRCodeProps } from '../@types'
+
+function QRCode({
+  title,
+  description,
+  imageUrl,
+}: QRCodeProps) {
   return (
     <div className="w-80 bg-white p-4 rounded-3xl text-center shadow-lg">
-      <img className="w-81 h-81" src="./images/image-qr-code.png" alt="QR Code" />
+      <img className="w-81 h-81" src={imageUrl} alt="QR Code" />
 
       <div className="mt-6 px-4 pb-7">
-        <h2 className="font-bold text-2xl leading-6">Improve your front-end skills by building projects</h2>
-        <p className="mt-3 text-sm leading-4 text-grayish-blue">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+        <h2 className="font-bold text-2xl leading-6">
+          {title}
+        </h2>
+        <p className="mt-3 text-sm leading-4 text-grayish-blue">
+          {description}
+        </p>
       </div>
     </div>
   )
